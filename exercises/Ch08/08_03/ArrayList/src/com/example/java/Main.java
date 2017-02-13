14a271a8a75d4f1ae27c21cc473f6618295b398f
ardydedase <ardy.dedase@gmail.com>
2017-02-13 22:20:29 +0800

    data collections

--- exercises/Ch08/08_03/ArrayList/src/com/example/java/Main.java
+++ exercises/Ch08/08_03/ArrayList/src/com/example/java/Main.java
@@ -1,11 +1,33 @@
 package com.example.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // set the size for a more efficient allocation of memory
+        List<String> list = new ArrayList<>();
 
+        list.add("California");
+        list.add("Oregon");
+        list.add("Washington");
 
+        // toString method is automatically called in the object
+        System.out.println(list);
+
+        list.add("Alaska");
+        System.out.println(list);
+
+        list.remove(0);
+        System.out.println(list);
+
+        String state = list.get(1);
+        System.out.println("The second state is " + state);
+
+        int pos = list.indexOf("Alaska");
+        System.out.println("Alaska is at position " + pos);
     }
 
 }
--- exercises/Ch08/08_04/HashMap/src/com/example/java/Main.java
+++ exercises/Ch08/08_04/HashMap/src/com/example/java/Main.java
@@ -1,11 +1,29 @@
 package com.example.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
-		
+        // implementation of interface named Map
+        // no need to re-declare the data type of key and value
+        Map<String, String> map = new HashMap<>();
 
+        map.put("California", "Sacramento");
+        map.put("Oregon", "Salem");
+        map.put("Washington", "Olympia");
+        System.out.println(map);
+
+        map.put("Alaska", "Juneau");
+        System.out.println(map);
+
+        String cap = map.get("Oregon");
+        System.out.println("The capital of Oregon is " + cap);
+
+        map.remove("California");
+        System.out.println(map);
     }
 
 }

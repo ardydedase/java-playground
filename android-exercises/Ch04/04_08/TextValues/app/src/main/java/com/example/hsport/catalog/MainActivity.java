f6632eff8c5b3a5895e6853c5eb199347fc2c1d7
ardydedase <ardy.dedase@gmail.com>
2017-03-07 23:05:16 +0800

    Display and collect text values.

--- android-exercises/Ch04/04_08/TextValues/app/src/main/java/com/example/hsport/catalog/MainActivity.java
+++ android-exercises/Ch04/04_08/TextValues/app/src/main/java/com/example/hsport/catalog/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hsport.catalog;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,10 +26,21 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                EditText et = (EditText) findViewById(R.id.nameText);
+                String entry = et.getText().toString();
+
+                Snackbar.make(view, "You entered: " + entry, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            builder.append(getString(R.string.lorem_ipsum) + "\n\n");
+        }
+        TextView tv = (TextView) findViewById(R.id.longText);
+        tv.setText(builder.toString());
 
     }
 
